f3c34245c1d18091efe743baf0b668c220153f2f
reizergrzegorz <reizergrzegorz@gmail.com>
2022-08-22 13:20:07 +0200

    feat_add_walls_to_board

--- engine.py
+++ engine.py
@@ -9,7 +9,14 @@ def create_board(width, height):
     Returns:
     list: Game board
     '''
-    return [[' ' for x in range(height)] for y in range(width)]
+    board = [[' ' for x in range(height)] for y in range(width)]
+    for i in range(len(board)):
+        board[i][height-1] = '#' 
+        board[width-1][i] = '#'
+        board[i][0] = '#'
+        board[0][i] = '#'    
+    return board
+
 
 
 def put_player_on_board(board, player):
