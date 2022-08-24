961ec3b99e3255aff9f2d5283e83099c385da924
PrzemekKomorowski <106514184+PrzemekKomorowski@users.noreply.github.com>
2022-08-24 14:33:12 +0200

    Dodany system walki oraz wymiany ekwipunku

--- engine.py
+++ engine.py
@@ -18,8 +18,8 @@ def put_player_on_board(board, player):
 def remove_player_from_board(board, player):
     board[player['y']][player['x']] = ' '
 
-def player_movement(key, board, player):
-    fields = [' ', '#']
+def player_movement(key, board, player,items):
+    
     
     if key == 'q':
         exit()
@@ -28,33 +28,35 @@ def player_movement(key, board, player):
     elif key == 'w':
         if board[player['y']-1][player['x']] != '#':
             player['y'] -= 1
-            if board[player['y']][player['x']] not in fields:
+            if board[player['y']][player['x']] in items:
                 return board[player['y']][player['x']]                
     elif key == 's':
         if board[player['y']+1][player['x']] != '#':
             player['y'] += 1
-            if board[player['y']][player['x']] not in fields:
+            if board[player['y']][player['x']] in items:
                 return board[player['y']][player['x']]
     elif key == 'a':
         if board[player['y']][player['x']-1] != '#':
             player['x'] -= 1
-            if board[player['y']][player['x']] not in fields:
+            if board[player['y']][player['x']] in items:
                 return board[player['y']][player['x']]
     elif key == 'd':
         if board[player['y']][player['x']+1] != '#':
             player['x'] += 1
-            if board[player['y']][player['x']] not in fields:
+            if board[player['y']][player['x']] in items:
                 return board[player['y']][player['x']]
 
 def add_to_inventory(inventory, field, player):
     if field == "*":
-        player['player_health'] += 10
+        player['player_health'] += 4
+    elif field == "<":
+        inventory_exchange(player,inventory,item_value,"wooden_helmet")
+    elif field == ">":
+        inventory_exchange(player,inventory,item_value,"steel_helmet")
     elif field == "^":
-        inventory.append("Helmet")
-        player['player_defense'] += 3
+        inventory_exchange(player,inventory,item_value,"enchanted_helmet")
     elif field == "/":
-        inventory.append("Sword")
-        player['player_attack'] += 5
+        inventory_exchange(player,inventory,item_value,"wooden_sword")
     elif field == "$":
         inventory.append("Key")
     return inventory, player
@@ -83,7 +85,7 @@ def create_enemy_2(position_list):
         "x" : random.choice(enemy2_x_place),
         "y" : random.choice(enemy2_y_place),
         "cpu_health" : 10,
-        "attack_power": 8,
+        "attack_power": 20,
         "defence": 2}
     check_position(position_list,cpu_2)
     position_list.append((cpu_2["y"],cpu_2["x"]))
@@ -114,21 +116,51 @@ def put_bread_on_board(board, object):
     board[object["y"]][object["x"]] = object["bread_icon"]
     return board
 
-def create_helmet(position_list):
+def create_wooden_helmet(position_list):
     x_place = [2,3,4,5,6,7,8]
     y_place = [2,3,4,5,6,7,8]
 
-    helmet = {
-        "helmet_icon" : "^",
+    wooden_helmet = {
+        "wooden_helmet_icon" : "<",
         "x" : random.choice(x_place),
-        "y" : random.choice(y_place),
-        "defence_add": 3}
-    check_position(position_list,helmet)
-    position_list.append((helmet["y"],helmet["x"]))
-    return helmet
+        "y" : random.choice(y_place),}
+    check_position(position_list,wooden_helmet)
+    position_list.append((wooden_helmet["y"],wooden_helmet["x"]))
+    return wooden_helmet
+
+def create_steel_helmet(position_list):
+    x_place = [2,3,4,5,6,7,8]
+    y_place = [2,3,4,5,6,7,8]
+
+    steel_helmet = {
+        "steel_helmet_icon" : ">",
+        "x" : random.choice(x_place),
+        "y" : random.choice(y_place)}
+    check_position(position_list,steel_helmet)
+    position_list.append((steel_helmet["y"],steel_helmet["x"]))
+    return steel_helmet
+
+def create_enchanted_helmet(position_list):
+    x_place = [2,3,4,5,6,7,8]
+    y_place = [2,3,4,5,6,7,8]
+
+    enchanted_helmet = {
+        "enchanted_helmet_icon" : "^",
+        "x" : random.choice(x_place),
+        "y" : random.choice(y_place)}
+    check_position(position_list,enchanted_helmet)
+    position_list.append((enchanted_helmet["y"],enchanted_helmet["x"]))
+    return enchanted_helmet
+
 
 def put_helmet_on_board(board, object):
-    board[object["y"]][object["x"]] = object["helmet_icon"]
+    board[object["y"]][object["x"]] = object["wooden_helmet_icon"]
+    return board
+def put_helmet2_on_board(board, object):
+    board[object["y"]][object["x"]] = object["steel_helmet_icon"]
+    return board
+def put_helmet3_on_board(board, object):
+    board[object["y"]][object["x"]] = object["enchanted_helmet_icon"]
     return board
 
 def create_sword(position_list):
@@ -162,3 +194,115 @@ def fight_with_NPC(NPC: str, player:str):
         NPC['cpu_health'] = NPC['cpu_health'] - (player['player_attack'] - NPC['defence'])
     if (NPC['attack_power'] - player['player_defense']) >= 0:
         player['player_health'] = player['player_health'] - (NPC['attack_power'] - player['player_defense'])
+
+
+def inventory_value():
+    item_value = {
+        "wooden_sword" : 2,
+        "steel_sword" : 4,
+        "bow" : 5,
+        "wooden_helmet" : 2,
+        "steel_helmet" : 4,
+        "enchanted_helmet" : 6,
+        "wooden_armor" : 2,
+        "steel_armor" : 4,
+        "diamond_armor" : 6,
+        "shield" : 4,
+        "Magic Scroll" : 2,
+        "Water Magic Scroll" : 4,
+        "Fire Magic Scroll" : 6,
+        "heal_potion" : 5,
+        "strength_potion" : 5,
+        "power_potion" : 5,
+        }
+    return item_value
+item_value = inventory_value()
+sword_items = ["wooden_sword","steel_sword","bow"]
+helmet_items = ["wooden_helmet","steel_helmet","enchanted_helmet"]
+armor_items = ["wooden_armor","steel_armor","diamond_armor"]
+shield_items = ["shield"]
+scroll_items = ["Magic Scroll","Water Magic Scroll","Fire Magic Scroll"]
+potion_items = ["heal_potion","strength_potion","power_potion","death_potion"]
+
+
+def inventory_exchange(player,inventory:list,item_value,item):
+    if item in sword_items:
+        if player["equipped_sword"] == "":
+            print(item_value[item])
+            player["equipped_sword"] = item
+            player["player_attack"] += item_value[item]
+            inventory.append(item)
+            return player
+        else:
+            player["player_attack"] -= item_value[player["equipped_sword"]]
+            player["player_attack"] += item_value[item]
+            player["equipped_sword"] = item
+            inventory.append(item)
+            return player
+    elif item in helmet_items:
+        if player["equipped_helmet"] == "":
+            print(item_value[item])
+            print(item)
+            player["equipped_helmet"] = item
+            player["player_defense"] += item_value[item]
+            inventory.append(item)
+            return player
+        else:
+            player["player_defense"] -= item_value[player["equipped_helmet"]]
+            player["player_defense"] += item_value[item]
+            player["equipped_helmet"] = item
+            inventory.append(item)
+            return player
+    elif item in armor_items:
+        if player["equipped_armor"] == "":
+            player["equipped_armor"] = item
+            player["player_defense"] += item_value[item]
+            inventory.append(item)
+            return player
+        else:
+            player["player_defense"] -= item_value[player["equipped_armor"]]
+            player["player_defense"] += item_value[item]
+            player["equipped_armor"] = item
+            inventory.append(item)
+            return player
+    elif item in shield_items:
+        if player["equipped_shield"] == "":
+            player["equipped_shield"] = item
+            player["player_defense"] += item_value[item]
+            inventory.append(item)
+            return player
+        else:
+            player["player_defense"] -= item_value[player["equipped_shield"]]
+            player["player_defense"] += item_value[item]
+            player["equipped_shield"] = item
+            inventory.append(item)
+            return player
+    elif item in scroll_items:
+        if player["equipped_weapon2"] == "":
+            player["equipped_weapon2"] = item
+            player["player_attack"] += item_value[item]
+            inventory.append(item)
+            return player
+        else:
+            player["player_attack"] -= item_value[player["equipped_weapon2"]]
+            player["player_attack"] += item_value[item]
+            player["equipped_weapon2"] = item
+            inventory.append(item)
+            return player
+    elif item in potion_items:
+        if item == "heal_potion":
+            player["player_health"] += 10
+            return player
+        elif item == "strength_potion":
+            player["player_health"] += 2
+            player["player_attack"] += 5
+            player["player_defense"] += 5
+            return player
+        elif item == "power_potion":
+            player["player_health"] += 8
+            player["player_attack"] += 2
+            player["player_defense"] += 2
+            return player
+        elif item == "death_potion":
+            player["player_health"] = 0
+            return player
--- main.py
+++ main.py
@@ -8,12 +8,41 @@ height = 10
 
 def create_player():
     player = {
+        "player_name" : "",
         "player_icon" : "@",
         "x" : 1,
         "y" : 1,
-        "player_health" : 5,
-        "player_attack": 2,
-        "player_defense" : 3}
+        "player_health" : 0,
+        "player_attack": 0,
+        "player_defense" : 0,
+        "equipped_sword" : "",
+        "equipped_helmet" : "",
+        "equipped_armor" : "",
+        "equipped_weapon2" : "",
+        "equipped_potion" : "",
+        }
+    player["player_name"] = input('Player name: ').capitalize()
+    while True:
+        print('Type lists: Dwarf(1), Elves(2), Human(3)')
+        type_lists = ("1", "2", "3")
+        player_type = input("Please choose your type (1 or 2 or 3): ")
+        if player_type not in type_lists:
+            print("Wrong input, please choose 1, 2 or 3")
+            continue
+        else:
+            break 
+    if player_type == "1":
+        player["player_health"] = 20
+        player["player_attack"] = 3
+        player["player_defense"] = 6
+    elif player_type == "2":
+        player["player_health"] = 14
+        player["player_attack"] = 6
+        player["player_defense"] = 3
+    elif player_type == "3":
+        player["player_health"] = 15
+        player["player_attack"] = 4
+        player["player_defense"] = 4 
     return player
 
 def main():
@@ -23,18 +52,18 @@ def main():
     cpu_1 = engine.create_enemy(position_list)
     cpu_2 = engine.create_enemy_2(position_list)
     bread = engine.create_bread(position_list)
-    helmet = engine.create_helmet(position_list)
+    wooden_helmet = engine.create_wooden_helmet(position_list)
     sword = engine.create_sword(position_list)
     board = engine.create_board(width, height)
 
     mobs = ['m', 'M']
-    items = ['*', '/', '^', '$']
+    items = ['/','*','$','<','>','^','m','M']
     util.clear_screen()
     is_running = True
     engine.put_cpu1_on_board(board,cpu_1)
     engine.put_cpu2_on_board(board, cpu_2)
     engine.put_bread_on_board(board, bread)
-    engine.put_helmet_on_board(board, helmet)
+    engine.put_helmet_on_board(board, wooden_helmet)
     engine.put_sword_on_board(board, sword)
     while is_running:
         engine.put_player_on_board(board, player)
@@ -42,13 +71,12 @@ def main():
         engine.remove_player_from_board(board, player)
 
         key = util.key_pressed()
-        field = engine.player_movement(key, board, player)
+        field = engine.player_movement(key, board, player,items)
         if field in items:
             engine.add_to_inventory(inventory, field, player)
-        if field in mobs:
-            if field == 'm':
+        if field == 'm':
                 engine.fight_with_NPC(cpu_1, player)
-            elif field == 'M':
+        elif field == 'M':
                 engine.fight_with_NPC(cpu_2, player)
         if player['player_health'] <= 0:
             print("You have died!")
